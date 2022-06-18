1afc5b49d7a7eef92f0f5370c2ce56f4ec680b26
Sandra <99267898+Sandra-Silva-Santos@users.noreply.github.com>
2022-06-18 00:38:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+});
